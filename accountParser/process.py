6eb0fc40e89a5d0efc1284c3cadd2e89738f4c2c
Thomas Meehan <thom.meehan@gmail.com>
2024-09-06 12:08:00 +0100

    Small updates

--- accountParser/process.py
+++ accountParser/process.py
@@ -30,23 +30,29 @@ def files():
 # process an individual file 
 # the name could be the actual file name initially and the file name as recorded in the 
 # example file 9e6295b0-2d43-4578-9968-6f5233699060.csv
-@bp.route('/file/<filename>')
+@bp.route('/file/<filename>', methods=('GET', 'POST'))
 def processFile(filename):
-    uploadsPath = f"{os.getcwd()}/uploads"
-
     lines = []
-    try:
-        with open(f"{uploadsPath}/{filename}") as csvfile:
-            fileReader = csv.reader(csvfile)
-            for row in fileReader:
-                # print(', '.join(row))
-            #    line = []
-                lines.append(row)
+    if request.method == 'POST':
+        # deal with the form
+        print(request.form)
+        processed = True
+    else:
+        uploadsPath = f"{os.getcwd()}/uploads"
 
-#                print(line)
-#            lines.append(row)
-    except FileNotFoundError:
-        print(f"{filename}: does not exist")
+        try:
+            with open(f"{uploadsPath}/{filename}") as csvfile:
+                fileReader = csv.reader(csvfile)
+                for row in fileReader:
+                    lines.append(row)
+        except FileNotFoundError:
+            print(f"{filename}: does not exist")
+
+        for line in lines:
+            line.append(''.join(line))
+            #print(hash(joinedRow))
+
+        processed = False
 
     print(lines)
-    return render_template('process/file.html', file=filename, lines=lines)
+    return render_template('process/file.html', processed=processed, file=filename, lines=lines)
