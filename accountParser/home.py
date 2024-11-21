93244183d9ac63fc4ae97e4f650dc06f3124f7bb
Thomas Meehan <thom.meehan@gmail.com>
2024-11-21 18:24:48 +0000

    Got em on the home page!

--- accountParser/home.py
+++ accountParser/home.py
@@ -3,6 +3,8 @@ from flask import (
 )
 
 from accountParser.db import get_db
+from accountParser.categories import getCategories
+import json
 
 class file():
     def __init__(self, name, location):
@@ -25,9 +27,38 @@ def getCategoryByTime(month, year):
     ).fetchall()
     return categories
 
+def getCatSumByIDandTime(id, month, year):
+
+    db = get_db()
+    categorySum = db.execute(
+         '''
+        SELECT SUM(accountEntries.amount) AS amount 
+        FROM accountEntries 
+        JOIN categories ON accountEntries.category_id=categories.id 
+        WHERE accountEntries.month = ? AND accountEntries.year = ? AND accountEntries.category_id = ?
+        ''',
+        (month, year, id)
+    ).fetchall()
+    return categorySum
+
 @bp.route('/', methods=('GET', 'POST'))
 def files():
+    categoriesObj = { "categories": [] }
     categories = []
+    print(getCategories())
+    for category in getCategories():
+        categoryArr = []
+        categoryArr.append(category['title'])
+        for i in range(1, 13):
+            print(i)
+            monthSum = getCatSumByIDandTime(category['id'], i, 2024)
+            print(monthSum[0]['amount'])
+            categoryArr.append(monthSum[0]['amount'])
+
+        categories.append(categoryArr)
+
+
+    '''
     month = False
     year = False
     if request.args.get("month") or request.args.get('year'):
@@ -38,5 +69,5 @@ def files():
         print(month)
         categories = getCategoryByTime(month, year)
         print(categories)
-        
+    '''
     return render_template('home/index.html', categories=categories)
